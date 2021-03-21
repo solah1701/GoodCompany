ae95b89bfe82c6dc79fa647267d9a670eacc9174
Stephen Olah <solah1701@gmail.com>
2021-03-21 22:03:09 +1100

    -Added DeviceController -Added first Device View -Updated layout to contain link to inventory

---
+++ GoodCompany/Controllers/DeviceController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodCompany.Controllers
+{
+    public class DeviceController : Controller
+    {
+        private List<Models.DeviceItem> model;
+
+        // GET: DeviceController
+        public ActionResult Index()
+        {
+            if (model == null) model = new List<Models.DeviceItem>();
+            return View(model);
+        }
+
+        // GET: DeviceController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DeviceController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DeviceController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DeviceController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DeviceController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DeviceController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DeviceController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
