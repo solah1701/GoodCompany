6b0a9a429e330c3faf581171a6610dc946c5df57
Stephen Olah <solah1701@gmail.com>
2021-04-05 13:22:29 +1000

    - added Computer as the base inventory model and laptop as a derived inventory model

---
+++ GoodCompany/Models/Computer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodCompany.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public string ComputerType { get; set; }
+        public string Processor { get; set; }
+        public string Brand { get; set; }
+        public int UsbPorts { get; set; }
+        public int RamSlots { get; set; }
+        public string FormFactor { get; set; }
+        public int Quantity { get; set; }
+    }
+}
